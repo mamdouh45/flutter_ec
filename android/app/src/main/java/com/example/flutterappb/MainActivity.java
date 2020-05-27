2a520902df4f8954a9776c5efcbf6cb866658db9
mamdouh <3dhousedecor.com@gmail.com>
2020-05-27 12:42:47 -0700

    first app commerce

---
+++ android/app/src/main/java/com/example/flutterappb/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutterappb;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'dart.dart';
+
+void main() {
+print(name());
+}
